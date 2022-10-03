580b5bf5d6663a40d1bb05e25b7934f5c97d0317
amartingir <113341165+AritzMartin@users.noreply.github.com>
2022-10-03 10:14:18 +0200

    Ejercicio5 terminado

---
+++ Ejercicio5.py
@@ -0,0 +1,3 @@
+num_horas = float(input('Introduce el numero de horas trabajadas\n'))
+precio_hora = float(input('Introduce el numero de horas trabajadas\n'))
+print('Te corresponden', num_horas*precio_hora, '€')
