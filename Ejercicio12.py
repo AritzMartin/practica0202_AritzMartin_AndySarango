57dbf7bbc4029bf3725eaf8cc04bf722bcc47ab5
amartingir <113341165+AritzMartin@users.noreply.github.com>
2022-10-03 11:57:58 +0200

    Ejercicio12 Terminado

---
+++ Ejercicio12.py
@@ -0,0 +1,7 @@
+print('Cuantas barras que no son del dia se han vendido?')
+cantidad_barras = float(input())
+print('El precio de cada barra es de 3.49€')
+print('El descuento por no ser del dia es del 60%')
+precio = 3.49*0.6
+precio_final = round(precio, 2)
+print('El precio final es de', precio_final, '€')
